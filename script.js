4659a30d1a7e4046d6fbf154416df9167854da3e
senatsu-graphics <senatsugraphics@gmail.com>
2026-04-07 12:32:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const quizData = [
+  { word: "apple", hint: "fruit" },
+  { word: "beautiful", hint: "very pretty" },
+  { word: "because", hint: "reason" }
+];
+
 function playWord(word) {
   const utterance = new SpeechSynthesisUtterance(word);
   utterance.lang = "en-US";
   speechSynthesis.speak(utterance);
 }
+
+playAudioBtn.addEventListener("click", () => {
+  playWord(quizData[currentIndex].word);
+});
